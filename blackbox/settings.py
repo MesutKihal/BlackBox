d4759b238135dfd3a41b22e4490e259d09c05d46
Messaoud Kihal <messaoudkihal@protonmail.com>
2025-05-21 13:00:52 +0100

    preparing for vercel

--- blackbox/settings.py
+++ blackbox/settings.py
@@ -14,7 +14,7 @@ ENVIRONMENT = "production"
 DEBUG = True
 SECRET_KEY = "django-insecure-v!w2vzv(s$598ze4nqu!e=ri02)@@o1!xnw-y*3)lit)+=*^g&"
 DATABASE_URL = "postgresql://neondb_owner:npg_GF4ZvDeXiah9@ep-bold-pond-a4pnnhhk-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
-ALLOWED_HOSTS = ["blackbox-j4e5.onrender.com", "localhost", "127.0.0.1", "blackboxisdesign.com"]
+ALLOWED_HOSTS = ["blackbox-j4e5.onrender.com", "localhost", "127.0.0.1", "blackboxisdesign.com", ".vercel.app"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
